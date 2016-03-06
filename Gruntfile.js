eb452aeb1849516f2cf6f814bd1a4cb506238ad0
Nikolay Rodionov <nikolay@streamroot.io>
2016-03-06 20:06:32 +0100

    added grunt build scripts, and uglification scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,14 +10,45 @@ module.exports = function(grunt) {
                 src: 'lib/vjs-hls.js',
                 dest: 'debug/vjs-hls.js',
                 options:  {
-                  transform: ['babelify'],
-                  browserifyOptions: {
-                      debug: true
-                  },
-                  watch: true,
-                  keepAlive: true
-               }
+                    transform: ['babelify'],
+                    browserifyOptions: {
+                        debug: true
+                    },
+                    watch: true,
+                    keepAlive: true
+                }
+            },
+            dist: {
+                src: 'lib/vjs-hls.js',
+                dest: 'dist/vjs-hls.js',
+                options:  {
+                    transform: ['babelify'],
+                    browserifyOptions: {
+                        debug: false,
+                    },
+                    watch: false,
+                    keepAlive: false,
+                }
+            }
+        },
+        uglify: {
+            options: {
+                mangle: true,
+                compress: {
+                    drop_console: true
+                },
+                beautify: false
+            },
+            dist: {
+                files: {
+                    'dist/vjs-hls.min.js': 'dist/vjs-hls.js'
+                }
             }
         }
     });
+
+
+    grunt.registerTask('build', 'build dist scripts', ['browserify:dist', 'uglify:dist']);
+
+
 }
